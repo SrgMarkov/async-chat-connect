8e77d1152b951990590dc8b2cdec8b22e7c04029
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-01-30 20:16:22 +0400

    Add CLI options

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import argparse
 import asyncio
-import aiofiles
 import datetime
+import os
+
+import aiofiles
+from dotenv import load_dotenv
 
 
-async def get_chat_data():
+async def get_chat_data(host, port, save_file):
     while True:
         try:
-            reader, writer = await asyncio.open_connection(host='minechat.dvmn.org', port=5000)
+            reader, writer = await asyncio.open_connection(host=host, port=port)
             current_time = datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
             message = await reader.readline()
-            async with aiofiles.open('chat_history.txt', mode='a+') as history_file:
+            async with aiofiles.open(save_file, mode='a+') as history_file:
                 await history_file.write(f'[{current_time}] {message.decode()}')
 
             print(f'[{current_time}] {message.decode()}')
-        except Exception as error:
+        except OSError as error:
             print(f'Error: {error}')
 
 
 
 if __name__ == '__main__':
-    asyncio.run(get_chat_data())
+    load_dotenv()
+    command_arguments = argparse.ArgumentParser(description='Скрипт подключения к подпольному чату с возможностью сохранения переписки')
+    command_arguments.add_argument('--host', help='Укажите хост чата', default=os.getenv('HOST'))
+    command_arguments.add_argument('--port', help='Укажите порт чата', default=int(os.getenv('PORT')), type=int)
+    command_arguments.add_argument('--file', help='Укажите путь к файлу для сохранения переписки', default=os.getenv('HISTORY_FILE'))
+    args = command_arguments.parse_args()
+    asyncio.run(get_chat_data(args.host, args.port, args.file))
