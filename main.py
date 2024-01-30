cc774f572af17e6b221489eca3dfe18c768872a7
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-01-30 18:38:19 +0400

    Save all messages to file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import asyncio
+
+
+async def get_chat_data():
+    reader, writer = await asyncio.open_connection(host='minechat.dvmn.org', port=5000)
+    while True:
+        data = await reader.readline()
+        print(data.decode())
+
+
+if __name__ == '__main__':
+    asyncio.run(get_chat_data())
